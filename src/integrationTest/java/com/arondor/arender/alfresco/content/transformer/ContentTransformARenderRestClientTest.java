2ee538779d5a09cfc61f34021816a409ed9cf471
Lars Vierbergen <lars.vierbergen@xenit.eu>
2020-09-10 11:28:21 +0200

    [AGFASLA-63] Add test for arender rest client

---
+++ src/integrationTest/java/com/arondor/arender/alfresco/content/transformer/ContentTransformARenderRestClientTest.java
@@ -0,0 +1,45 @@
+package com.arondor.arender.alfresco.content.transformer;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.UUID;
+import org.apache.commons.io.Charsets;
+import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ContentTransformARenderRestClientTest {
+
+    private ContentTransformARenderRestClient aRenderRestClient;
+
+    @BeforeEach
+    public void setupRestClient() {
+        aRenderRestClient = new ContentTransformARenderRestClient();
+        aRenderRestClient.setAddress(System.getProperty("arender.address"));
+    }
+
+    @Test
+    public void testWeatherPerformance() {
+        assertTrue(aRenderRestClient.getWeatherPerformance() > 0, "Weather performance should be larger than 0");
+    }
+
+    @Test
+    public void testRenderSmallDocument() throws IOException {
+        UUID randomID = UUID.randomUUID();
+        try (InputStream testDocument = ContentTransformARenderRestClientTest.class
+                .getResourceAsStream("lorem10.doc")) {
+            aRenderRestClient.uploadDocument(randomID.toString(), testDocument, "application/msword");
+        }
+
+        try (InputStream result = aRenderRestClient.getInputStream(randomID.toString(), "RENDERED")) {
+            byte[] bytes = new byte[5];
+            IOUtils.readFully(result, bytes);
+            // Check that returned document starts with the PDF header
+            assertArrayEquals("%PDF-".getBytes(Charsets.US_ASCII), bytes);
+        }
+    }
+
+}
